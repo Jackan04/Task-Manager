aef78cebd2679fba405c975e4fc23095e080ef6a
Jackan04 <jacob.asker@icloud.com>
2025-05-31 09:49:24 +0200

    - Installed and configured html-webpack-plugin - Created the sidebar in the template.html file - Added initial css variables - Applied basic styling to the sidebar

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // Optional: cleans /dist on every build
   },
   mode: 'development',
   devServer: {
@@ -14,9 +16,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,     
-        use: ['style-loader', 'css-loader'], 
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
 };
